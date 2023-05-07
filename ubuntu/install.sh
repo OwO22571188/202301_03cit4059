c6837094ef18ed9c37ddafa2ff1f76fec6120249
Garrick Ho <sales@cherryec.com>
2023-05-07 18:55:44 +0800

    Updated

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,5 +1,7 @@
-# Install docker
 sudo apt update
+# Install git
+sudo apt install -y git
+# Install docker
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
